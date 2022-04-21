b66855b5ecd49bd995a691146ec7ab04c3a65be5
Sakkarius <101215611+Sakkarius@users.noreply.github.com>
2022-04-21 23:32:36 +0000

    Skilaverkefni
    Magic 8 ball verkefni

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
+          appBar: AppBar(
+            title: Center(child: Text('Ask Me Anything')),
+            backgroundColor: Colors.blue,
+          ),
+          body: Ball(),
+        ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  const Ball({Key key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int randomNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    Random rng = new Random();
+                    randomNumber = rng.nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$randomNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
